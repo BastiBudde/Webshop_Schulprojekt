a057b2316b52e4a422ee8e622395d6d071e2ba1e
Basti Budde <basti_budde@web.de>
2020-12-13 00:10:33 +0100

    Stand 12.12.2020 - Schule

--- backend/backend_uebersicht.php
+++ backend/backend_uebersicht.php
@@ -4,42 +4,90 @@
     include "../includes/siteHeader.php";
     include "../includes/siteFooter.php";
     include "../includes/connectToDB.php";
+    setlocale (LC_ALL, 'de_DE@euro', 'de_DE', 'de', 'ge');
 
     //////////Interessante Daten wie anzahl der Bestellungen, Summe der Auftragswerte usw. besorgen\\\\\\\\\\
 
-    /*  Anzahl an Bestellungen Summe aller Bestellungen holen und 
-        Anzahl nicht stornierter Bestellungen und Summe aller nicht stornierten Auftragswerte abfragen */
+    /*  Anzahl und Summe aller Bestellungen und 
+        Anzahl und Summe aller nicht stornierter Bestellungen und
+        Anzahl und Summe aller abgeschlossenen Bestellungen abfragen */
     $sql = "SELECT COUNT(ID_Bestellung), SUM(wert)
             FROM bestellung
             UNION
             SELECT COUNT(ID_Bestellung), SUM(wert)
             FROM bestellung
-            WHERE status NOT LIKE 'Storniert';";
+            WHERE status NOT LIKE 'Storniert'
+            UNION
+            SELECT COUNT(ID_Bestellung), SUM(wert)
+            FROM bestellung
+            WHERE status LIKE 'Versendet'
+                OR status LIKE 'Zugestellt';";
 
     $result = mysqli_query($dbh, $sql)
                         or die("Fehler bei der Query: ".mysqli_error($dbh));
                         
-    $anzBest_sumBest = mysqli_fetch_row($result);
-    $anzBest_sumBest_notStorniert = mysqli_fetch_row($result);
+    $total_CountAndSum_all = mysqli_fetch_row($result);
+    $total_CountAndSum_notStorniert = mysqli_fetch_row($result);
+    $total_CountAndSum_complete = mysqli_fetch_row($result);
 
     
 
-    /*  Anzahl der Bestellungen mit dem Status offen holen und
-        Anzahl der Bestellungen mit dem Status storniert abfragen */
+    /*  Anzahl der Bestellungen mit dem Status offen und
+        Anzahl der Bestellungen mit dem Status storniert und 
+        Anzahl der Bestellungen mit dem Status bestätigt und 
+        Anzahl der Bestellungen mit dem Status versendet abfragen */
     $sql = "SELECT COUNT(ID_Bestellung)
             FROM bestellung
-            WHERE status = 'In Bearbeitung'
-            UNION
-            SELECT COUNT(ID_Bestellung)
+            WHERE status = 'In Bearbeitung';";
+
+    $result = mysqli_query($dbh, $sql)
+                or die("Fehler bei der Query: ".mysqli_error($dbh));
+
+    $total_Count_offen = mysqli_fetch_row($result)[0];
+
+
+
+    $sql =  "SELECT COUNT(ID_Bestellung)
             FROM bestellung
             WHERE status = 'Storniert';";
 
     $result = mysqli_query($dbh, $sql)
+                or die("Fehler bei der Query: ".mysqli_error($dbh));
+
+    $total_Count_storniert = mysqli_fetch_row($result)[0];
+
+
+
+    $sql = "SELECT COUNT(ID_Bestellung)
+            FROM bestellung
+            WHERE status = 'Bestaetigt';";
+
+    $result = mysqli_query($dbh, $sql)
+                or die("Fehler bei der Query: ".mysqli_error($dbh));
+                
+    $total_Count_bestaetigt = mysqli_fetch_row($result)[0];
+        
+        
+
+    $sql = "SELECT COUNT(ID_Bestellung)
+            FROM bestellung
+            WHERE status = 'Zugestellt';";
+
+    $result = mysqli_query($dbh, $sql)
                         or die("Fehler bei der Query: ".mysqli_error($dbh));
     
-    $anzStatus_offen = mysqli_fetch_row($result)[0];
-    $anzStatus_storniert = mysqli_fetch_row($result)[0];
+    $total_Count_zugestellt = mysqli_fetch_row($result)[0];
 
+    
+
+    $sql = "SELECT COUNT(ID_Bestellung)
+    FROM bestellung
+    WHERE status = 'Versendet';";
+    
+    $result = mysqli_query($dbh, $sql)
+                or die("Fehler bei der Query: ".mysqli_error($dbh));
+
+    $total_Count_versendet = mysqli_fetch_row($result)[0];
 
 
     //////////\\\\\\\\\\
@@ -52,68 +100,167 @@
         echo "
         
         <div class='flex-DirCol'>
-            <div class='backend-newDBdataInput-container'>
+            <div class='flex-DirCol'>
 
-                    <table>
-
-                        <caption>
-                            <div>
-                                <h2> Willkommen " . $_SESSION['mitarbeiter_Vorname'] . " " . $_SESSION['mitarbeiter_Nachname'] . "! </h2> 
-                                
-                                <form action='backend-logout.php'>
-                                    <input type='submit' value='Logout' class='button buttonSmall'>
-                                </form>
-                            </div>
-                            <br>
-                        </caption>
+                <div class='flex-DirRow flex-SpaceBetween'>
+                    <h2> Willkommen " . $_SESSION['mitarbeiter_Vorname'] . " " . $_SESSION['mitarbeiter_Nachname'] . "! </h2> 
                     
-                        <tr>
-                            <td>
-                                <a href='backend_insertProduct.php' class='button buttonNormal'>Produkt einfügen</a>
-                                <br>
-                                <br>
-                            </td>
-                            <td>
-                                <a href='backend_search.php' class='button buttonNormal'>Produkt bearbeiten</a>
-                            </td>
-                        </tr>
-                    
-                    </table>
+                    <form action='backend-logout.php'>
+                        <input type='submit' value='Logout' class='button buttonSmall'>
+                    </form>
                 </div>
-                
-                <div class='backend-newDBdataInput-container'>
-                
-                    <table>
-                    
-                        <tr>
-                            <td>Anzahl aller Bestellungen:</td>
-                            <td>".$anzBest_sumBest[0]."</td>
-                        </tr>
-                        <tr>
-                            <td>Anzahl aller Bestellungen (nicht storniert):</td>
-                            <td>".$anzBest_sumBest_notStorniert[0]."</td>
-                        </tr>
-                        <tr>
-                            <td>Summe aller Auftragswerte:</td>
-                            <td>".$anzBest_sumBest[1]."€</td>
-                        </tr>
-                        <tr>
-                            <td>Summe aller Auftragswerte(nicht storniert):</td>
-                            <td>".$anzBest_sumBest_notStorniert[1]."€</td>
-                        </tr>
-                        <tr>
-                            <td>Offene Bestellungen:</td>
-                            <td>".$anzStatus_offen."</td>
-                        </tr>
-                        <tr>
-                            <td>Stornierte Bestellungen:</td>
-                            <td>".$anzStatus_storniert."</td>
-                        </tr>
-                    
-                    </table>
+                <br>
+                <div class='flex-DirRow flex-SpaceBetween'>    
+                    <a href='backend_insertProduct.php' class='button buttonNormal'>Produkt einfügen</a>
 
+                    <a href='backend_search.php' class='button buttonNormal'>Produkt bearbeiten</a>
                 </div>
-            </div>";
+
+            </div>  
+
+            <div>
+                <table class='backend-webshopStatsTable'>
+                    <caption><h2>Werte Monat " . strftime("%B", date("F")) . ":</h2></caption>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen:</td>
+                        <td id='data'>".$total_CountAndSum_all[0]."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte:</td>
+                        <td id='data'>".$total_CountAndSum_all[1]."€</td>
+
+                    </td>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen (nicht storniert):</td>
+                        <td id='data'>".$total_CountAndSum_notStorniert[0]."</td>
+                        
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte (nicht storniert):</td>
+                        <td id='data'>".$total_CountAndSum_notStorniert[1]."€</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen (abgeschlossen):</td>
+                        <td id='data'>".$total_CountAndSum_complete[0]."</td>
+                        
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte (abgeschlossen):</td>
+                        <td id='data'>".$total_CountAndSum_complete[1]."€</td>
+                    </tr>
+
+
+                    <tr id='lineTop'>
+                        <td id='name' class='borderTop'>Offene Bestellungen:</td>
+                        <td id='data' class='borderTop'>".$total_Count_offen."</td>
+
+                        <td id='spacing' class='borderTop'></td>
+                        <td id='spacing' class='borderTop borderLeft'></td>
+
+                        <td id='name' class='borderTop'>Bestätigte Bestellungen:</td>
+                        <td id='data' class='borderTop'>".$total_Count_bestaetigt."</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Versendete Bestellungen:</td>
+                        <td id='data'>".$total_Count_versendet."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+
+                        <td id='name'>Zugestellte Bestellungen:</td>
+                        <td id='data'>".$total_Count_zugestellt."</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Stornierte Bestellungen:</td>
+                        <td id='data'>".$total_Count_storniert."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                    </tr>
+                </table>
+            </div>
+
+            <div>
+                <table class='backend-webshopStatsTable'>
+                    <caption><h2>Werte Insgesamt:</h2></caption>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen:</td>
+                        <td id='data'>".$total_CountAndSum_all[0]."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte:</td>
+                        <td id='data'>".$total_CountAndSum_all[1]."€</td>
+
+                    </td>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen (nicht storniert):</td>
+                        <td id='data'>".$total_CountAndSum_notStorniert[0]."</td>
+                        
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte (nicht storniert):</td>
+                        <td id='data'>".$total_CountAndSum_notStorniert[1]."€</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Anzahl aller Bestellungen (abgeschlossen):</td>
+                        <td id='data'>".$total_CountAndSum_complete[0]."</td>
+                        
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                        
+                        <td id='name'>Summe aller Auftragswerte (abgeschlossen):</td>
+                        <td id='data'>".$total_CountAndSum_complete[1]."€</td>
+                    </tr>
+
+
+                    <tr id='lineTop'>
+                        <td id='name' class='borderTop'>Offene Bestellungen:</td>
+                        <td id='data' class='borderTop'>".$total_Count_offen."</td>
+
+                        <td id='spacing' class='borderTop'></td>
+                        <td id='spacing' class='borderTop borderLeft'></td>
+
+                        <td id='name' class='borderTop'>Bestätigte Bestellungen:</td>
+                        <td id='data' class='borderTop'>".$total_Count_bestaetigt."</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Versendete Bestellungen:</td>
+                        <td id='data'>".$total_Count_versendet."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+
+                        <td id='name'>Zugestellte Bestellungen:</td>
+                        <td id='data'>".$total_Count_zugestellt."</td>
+                    </tr>
+
+                    <tr>
+                        <td id='name'>Stornierte Bestellungen:</td>
+                        <td id='data'>".$total_Count_storniert."</td>
+
+                        <td id='spacing'></td>
+                        <td id='spacing' class='borderLeft'></td>
+                    </tr>
+                </table>
+            </div>
+            
+        </div>";
     }
     else
     {
